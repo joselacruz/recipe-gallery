4a4f791b41c81c16252a9587b9a0239d631bffcc
joselacruz <joselacruzperez@gmail.com>
2023-02-20 11:31:14 -0400

    add slider movil with media query js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const main = document.querySelector("main");
 const footer = document.querySelector("footer");
 const body = document.querySelector("body");
 
+var mediaQuery = window.matchMedia("(max-width:700px)");
+
 const allImg = [...gallery.querySelectorAll("img")];
 const allImgsrc = allImg.map((img) => {
   return [img.src];
@@ -12,45 +14,61 @@ const allImgsrc = allImg.map((img) => {
 gallery.addEventListener("click", showImg);
 
 function showImg(e) {
-  if (e.target && e.target.tagName == "IMG") {
-    const currentImg = e.target.src;
+  if (e.target && e.target.tagName == "IMG") { 
 
-    writeHtmlBigImg(currentImg);
+  function mediaQueryMovil () {
+    if(mediaQuery.matches) {
+      gallery.classList.toggle("big-img-movil");
+      e.target.scrollIntoView()
+      
+    }
 
-    const IMG = document.querySelector(".big-img-container div img");
-    const btnNext = document.querySelector(".btnNext");
-    const btnBack = document.querySelector(".btnBack");
-
-    btnNext.addEventListener("click", () => {
-    //indexImgSelect apartir de la imagen actual ayudat a mostrar la siguente o anterior posision del array 
-      const indexImgSelect = allImgsrc.findIndex((src) => src == IMG.src); 
-
-      if (indexImgSelect <= allImgsrc.length - 2) {
-        IMG.src = allImgsrc[indexImgSelect + 1];
+   
+    else{
+        const currentImg = e.target.src;
+    
+        writeHtmlBigImg(currentImg);
+    
+        const IMG = document.querySelector(".big-img-container div img");
+        const btnNext = document.querySelector(".btnNext");
+        const btnBack = document.querySelector(".btnBack");
+    
+        btnNext.addEventListener("click", () => {
+        //indexImgSelect apartir de la imagen actual ayuda a mostrar la siguente o anterior posision del array 
+          const indexImgSelect = allImgsrc.findIndex((src) => src == IMG.src); 
+    
+          if (indexImgSelect <= allImgsrc.length - 2) {
+            IMG.src = allImgsrc[indexImgSelect + 1];
+          }
+              // retormar la primera posicion del array de imagenes
+              //permite volver a empezar la primera imagen si haci lo desea 
+          else {
+            IMG.src = allImgsrc[0];
+          }
+        });
+      
+        btnBack.addEventListener("click", () => {
+        //currentIndexBack apartir de la imagen actual ayuda a mostrar la siguente o anterior posision del array 
+          let currentIndexBack = allImgsrc.findIndex((src) => src == IMG.src);
+    
+          if (currentIndexBack < allImgsrc.length && currentIndexBack >= 1) {
+            IMG.src = allImgsrc[currentIndexBack - 1];
+          } else {
+            // retormar la ultima posicion del array de imagenes
+              //permite volver a empezar la ultima imagen si haci lo desea 
+            IMG.src = allImgsrc[allImgsrc.length - 1];
+          }
+        });
       }
-          // retormar la primera posicion del array de imagenes
-          //permite volver a empezar la primera imagen si haci lo desea 
-      else {
-        IMG.src = allImgsrc[0];
-      }
-    });
-
-    btnBack.addEventListener("click", () => {
-    //currentIndexBack apartir de la imagen actual ayudat a mostrar la siguente o anterior posision del array 
-      let currentIndexBack = allImgsrc.findIndex((src) => src == IMG.src);
-
-      if (currentIndexBack < allImgsrc.length && currentIndexBack >= 1) {
-        IMG.src = allImgsrc[currentIndexBack - 1];
-      } else {
-        // retormar la ultima posicion del array de imagenes
-          //permite volver a empezar la ultima imagen si haci lo desea 
-        IMG.src = allImgsrc[allImgsrc.length - 1];
-      }
-    });
-  }
+    }
 
 
+  
+  mediaQueryMovil();
 }
+}
+
+
 
 
 function writeHtmlBigImg(src) {
